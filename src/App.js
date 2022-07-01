2904eeb902367fb0f475c582798b78895153c9c4
Jacob Shalabi <jshalabi03@gmail.com>
2022-06-30 21:06:40 -0500

    clean up, important changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Niran from "./components/Niran"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My name Kazmain Haider Shah <code>"edit src/App.js for changes"</code> and I love Niran the most.
-        </p>
-        <a
-          className="App-link"
-          href="https://pornhub.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>welcome to nfta</h1>
+        <Niran niranToggled={false} togglePicture=""/>
       </header>
     </div>
   );
