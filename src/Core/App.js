9ea19ca4e78c8129312286dcf6e2d146c17ca6de
jshalabi03 <jshalabi03@gmail.com>
2022-07-02 17:28:20 -0500

    updated structure

--- src/Core/App.js
+++ src/Core/App.js
@@ -1,6 +1,5 @@
-import Niran from "./components/Niran"
-import NavBar from "./components/NavBar/Navbar";
-import Background from "./assets/newvid.mp4";
+import NavBar from "../Components/NavBar/Navbar";
+import Background from "../Files/Videos/newvid.mp4";
 import './App.css';
 
 function App() {
@@ -8,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <video autoPlay loop muted style = {{
-           position: "absolute", width: "100%", left: "50%", top: "50%", height: "80%", objectFit: "cover", transform: "translate(-50%, -50%"}}>
+           position: "absolute", width: "100%", left: "50%", top: "50%", height: "77.43234%", objectFit: "cover", transform: "translate(-50%, -50%"}}>
           <source src = {Background} type = "video/mp4"/>
         </video>
         <NavBar />
