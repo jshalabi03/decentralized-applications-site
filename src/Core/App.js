9866c5b996cc71ffde15b96ae9cabae030cae681
Kazmain Shah <kshah228@illinois.edu>
2022-07-03 16:37:32 -0500

    Moved video tag to Header; Added home page message but looks bad

--- src/Core/App.js
+++ src/Core/App.js
@@ -1,5 +1,4 @@
 import NavBar from "../components/NavBar/Navbar";
-import Background from "../Files/Videos/newvid.mp4";
 import Header from "../components/Header/Header.js";
 import './App.css';
 
@@ -7,10 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <video autoPlay loop muted style = {{
-          position: "absolute", width: "100%", left: "50%", top: "60%", height: "100%", objectFit: "cover", transform: "translate(-50%, -50%", opacity: "0.8"}}>
-          <source src = {Background} type = "video/mp4"/>
-        </video>
         <NavBar />
         <Header />
       </header>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './Header.css';
-// import Background from '../src/Files/Videos/newvid.mp4';
+import Background from '../../Files/Videos/newvid.mp4';
 
 function Header() {
     return(
         <div className = 'header-container'>
-            <h1></h1>
             <p></p>
+            <video autoPlay loop muted style = {{
+                position: "absolute", width: "100%", left: "50%", top: "60%", height: "100%", objectFit: "cover", transform: "translate(-50%, -50%", opacity: "1", playbackRate: "50%"}}>
+                <source src = {Background} type = "video/mp4"/>
+            </video>
+            <h1>Why Decentralize?</h1>
         </div>
     );
 }
 
 export default Header;
 
-{/* <video autoPlay loop muted style = {{
-                position: "absolute", width: "100%", left: "50%", top: "60%", height: "100%", objectFit: "cover", transform: "translate(-50%, -50%", opacity: "0.8"}}>
-                <source src = {Background} type = "video/mp4"/>
-            </video> */}
