cf69c7d70ce02be282cea2133c2a7ee2e89b2f2e
Kazmain Shah <kshah228@illinois.edu>
2022-07-03 00:58:25 -0500

    Fixed error and adjusted background

--- src/Core/App.js
+++ src/Core/App.js
@@ -1,4 +1,4 @@
-import NavBar from "../Components/NavBar/Navbar";
+import NavBar from "../components/NavBar/Navbar";
 import Background from "../Files/Videos/newvid.mp4";
 import './App.css';
 
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <video autoPlay loop muted style = {{
-           position: "absolute", width: "100%", left: "0%", top: "0%", height: "100%", objectFit: "cover", transform: "translate(-50%, -50%", opacity: "0.8"}}>
+           position: "absolute", width: "100%", left: "50%", top: "60%", height: "100%", objectFit: "cover", transform: "translate(-50%, -50%", opacity: "0.8"}}>
           <source src = {Background} type = "video/mp4"/>
         </video>
         <NavBar />
